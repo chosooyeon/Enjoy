bf5b2f69637412aaf23d496507392f79da1250d9
DESKTOP-D0H63JU\sooyeon <1whtndus@naver.com>
2018-10-11 20:00:32 +0900

    list 추가

--- GraduationProject/app/src/main/java/com/example/sooyeon/graduationproject/tab/MemoFragement.java
+++ GraduationProject/app/src/main/java/com/example/sooyeon/graduationproject/tab/MemoFragement.java
@@ -2,19 +2,45 @@ package com.example.sooyeon.graduationproject.tab;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.sooyeon.graduationproject.CustomDialog;
 import com.example.sooyeon.graduationproject.R;
+//import com.example.sooyeon.graduationproject.firebase.FirebaseHelper;
+import com.example.sooyeon.graduationproject.login.MainActivity;
+import com.example.sooyeon.graduationproject.recyclerList.MyAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MemoFragement extends Fragment {
 
+    //인증정보 가져오기
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+    private EditText editContent;
+    private TextView txtEmail;
+    private TextView txtName;
+    private String selectedMemoKey;
+    private static FirebaseDatabase mFirebaseDatabase;
+
+    static {
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mFirebaseDatabase.setPersistenceEnabled(true);
+    }
+
+    //private TextView main_label;
     public MemoFragement() {
         // Required empty public constructor
     }
@@ -29,12 +55,26 @@ public class MemoFragement extends Fragment {
 
         //TODO 여기에 View를 찾고 이벤트를 등록하고 등등의 처리를 할 수 있다.
         //getView().findViewById(R.id.btn1).setOnClickListener();
-        // 글 쓰기 버튼
-        view.findViewById(R.id.btnPlus).setOnClickListener(new View.OnClickListener() {
+
+        //인스턴스를 얻어온다
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        editContent = view.findViewById(R.id.content);
+
+        if(mFirebaseUser ==null){
+            Intent i = new Intent(getActivity(),MainActivity.class);
+            startActivity(i);
+        }
+        FloatingActionButton fabNewMemo = view.findViewById(R.id.btnPlus);
+
+
+        // 저장을 여기에서
+        fabNewMemo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //추가해야한다......!!!
-                //CustomDialog customDialog = new CustomDialog(MemoFragement.this);
+                CustomDialog customDialog = new CustomDialog(getActivity());
+                customDialog.callFunction(editContent);
             }
         });
 
