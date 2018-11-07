68d820e936219931d9b99ae3fc53b5d4b5af38a4
DESKTOP-D0H63JU\sooyeon <1whtndus@naver.com>
2018-11-07 15:52:59 +0900

    정리중

--- GraduationProject/app/src/main/java/com/example/sooyeon/graduationproject/login/MainActivity.java
+++ GraduationProject/app/src/main/java/com/example/sooyeon/graduationproject/login/MainActivity.java
@@ -1,28 +1,14 @@
 package com.example.sooyeon.graduationproject.login;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Base64;
-import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.androidquery.AQuery;
-import com.example.sooyeon.graduationproject.R;
-import com.example.sooyeon.graduationproject.tab.MemoFragement;
 import com.example.sooyeon.graduationproject.tab.TabMainActivity;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -38,19 +24,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
-import com.kakao.auth.ISessionCallback;
-import com.kakao.auth.Session;
-import com.kakao.network.ErrorResult;
-import com.kakao.usermgmt.LoginButton;
-import com.kakao.usermgmt.UserManagement;
-import com.kakao.usermgmt.callback.LogoutResponseCallback;
-import com.kakao.usermgmt.callback.MeResponseCallback;
-import com.kakao.usermgmt.response.model.UserProfile;
-import com.kakao.util.exception.KakaoException;
-import com.kakao.util.helper.log.Logger;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import com.example.sooyeon.graduationproject.R;
 
 public class MainActivity extends AppCompatActivity
         implements GoogleApiClient.OnConnectionFailedListener{
@@ -65,15 +39,6 @@ public class MainActivity extends AppCompatActivity
     private SignInButton mSigninBtn;//google 로그인
     private GoogleApiClient mGoogleApiClient;
     private FirebaseAuth mFirebaseAuth;
-
-    private SessionCallback callback;
-    //TextView user_nickname; //user_email;
-    //CircleImageView user_img; //카카오톡 이미지
-    //LinearLayout success_layout;
-    //Button logout_btn;
-    LoginButton loginButton;//mbtnLogin
-
-    AQuery aQuery; //카카오톡 닉네임
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -101,19 +66,6 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        try {
-            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.e("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-
         joinId = (EditText) findViewById(R.id.joinId);
         joinPwd = (EditText) findViewById(R.id.joinPwd);
         mbtnLogin = findViewById(R.id.btnLogin);
@@ -140,55 +92,12 @@ public class MainActivity extends AppCompatActivity
 
 
         });
-
-        aQuery = new AQuery(this);
-        callback = new SessionCallback();
-        Session.getCurrentSession().addCallback(callback);
-        // 카카오톡 로그인 버튼
-        loginButton = (LoginButton)findViewById(R.id.com_kakao_login);
-        loginButton.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-
-                if(event.getAction() == MotionEvent.ACTION_DOWN){
-                    if(!isConnected()){
-                        Toast.makeText(MainActivity.this,"인터넷 연결을 확인해주세요.",Toast.LENGTH_SHORT).show();
-                    }
-                }
-
-                if(isConnected()){
-                    return false;
-                }else{
-                    return true;
-                }
-            }
-        });
-
-        if(Session.getCurrentSession().isOpened()){
-            requestMe();
-        }else{
-            //success_layout.setVisibility(View.GONE);
-            loginButton.setVisibility(View.VISIBLE);
-        }
-
     }//end onCreate
-
-    //인터넷 연결상태 확인
-    public boolean isConnected() {
-        ConnectivityManager cm = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo netInfo = cm.getActiveNetworkInfo();
-        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
-            return true;
-        }
-        return false;
-    }
 
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
-            return;
-        }else
+        super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == 100){
             //Toast.makeText(AuthActivity.this, "onActivityResult", Toast.LENGTH_LONG).show();
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
@@ -200,8 +109,12 @@ public class MainActivity extends AppCompatActivity
                 Toast.makeText(this,"인증에 실패하였습니다.",Toast.LENGTH_LONG).show();
             }
         }
-        super.onActivityResult(requestCode, resultCode, data);
 
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this,"인증에 실패하였습니다.",Toast.LENGTH_LONG).show();
     }
 
     private void firebaseWithGoogle(GoogleSignInAccount account){
@@ -222,84 +135,8 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-
-    private class SessionCallback implements ISessionCallback {
-
-        @Override
-        public void onSessionOpened() {
-            //access token을 성공적으로 발급 받아 valid access token을 가지고 있는 상태. 일반적으로 로그인 후의 다음 activity로 이동한다.
-            if(Session.getCurrentSession().isOpened()){ // 한 번더 세션을 체크해주었습니다.
-                requestMe();
-            }
-        }
-
-        @Override
-        public void onSessionOpenFailed(KakaoException exception) {
-            if(exception != null) {
-                Logger.e(exception);
-            }
-        }
-    }
-
-//    private void requestLogout() {
-//        success_layout.setVisibility(View.GONE);
-//        loginButton.setVisibility(View.VISIBLE);
-//        UserManagement.requestLogout(new LogoutResponseCallback() {
-//            @Override
-//            public void onCompleteLogout() {
-//                runOnUiThread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        Toast.makeText(MainActivity.this, "로그아웃 성공", Toast.LENGTH_SHORT).show();
-//                    }
-//                });
-//            }
-//        });
-//    }
-
-    private void requestMe() {
-        //success_layout.setVisibility(View.VISIBLE);
-        loginButton.setVisibility(View.GONE);
-
-        UserManagement.requestMe(new MeResponseCallback() {
-            @Override
-            public void onFailure(ErrorResult errorResult) {
-                Log.e("onFailure", errorResult + "");
-            }
-
-            @Override
-            public void onSessionClosed(ErrorResult errorResult) {
-                Log.e("onSessionClosed",errorResult + "");
-            }
-
-            @Override
-            public void onSuccess(UserProfile userProfile) {
-                Log.e("onSuccess",userProfile.toString());
-                Toast.makeText(MainActivity.this, userProfile.getNickname()+"님 환영합니다", Toast.LENGTH_SHORT).show();
-
-                Intent i = new Intent(MainActivity.this, TabMainActivity.class);
-                startActivity(i);
-                //user_nickname.setText(userProfile.getNickname());
-                //user_email.setText(userProfile.getEmail());
-                //aQuery.id(user_img).image(userProfile.getThumbnailImagePath()); // <- 프로필 작은 이미지 , userProfile.getProfileImagePath() <- 큰 이미지
-            }
-
-            @Override
-            public void onNotSignedUp() {
-                Log.e("onNotSignedUp","onNotSignedUp");
-            }
-        });
-    }
-
-    @Override
     protected void onDestroy() {
         super.onDestroy();
-        Session.getCurrentSession().removeCallback(callback);
     }
-
 
 }
